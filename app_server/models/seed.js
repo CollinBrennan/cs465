e191a3d3159c26bb143ed9bdfa1208ff1bcc0959
Collin Brennan <collinbrennan8@gmail.com>
2024-11-20 13:48:19 -0500

    Module 4 completed baseline

---
+++ app_server/models/seed.js
@@ -0,0 +1,19 @@
+// MongoDB connection and Trip schema
+const Mongoose = require('./db')
+const Trip = require('./travlr')
+
+// Parse data from json file
+var fs = require('fs')
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'))
+
+// Delete existing records and insert seed data
+const seedDB = async () => {
+  await Trip.deleteMany({})
+  await Trip.insertMany(trips)
+}
+
+// Close MongoDB connection
+seedDB().then(async () => {
+  await Mongoose.connection.close()
+  process.exit(0)
+})
